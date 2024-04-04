24895ac68cc1eaeb7a8e1a1b8e8815fb6b2440b8
Harshtank-io <tankharsh67@gmail.com>
2024-04-04 18:50:37 +0530

    minor bugs fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.thbpo3883ng"
+    "revision": "0.28n4d2oklsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
